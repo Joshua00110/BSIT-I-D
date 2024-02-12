5257ea535e6e08127a3aa5592b390fdc5fea97a8
piniliace <158040288+piniliace@users.noreply.github.com>
2024-02-12 19:02:41 +0800

    Add files via upload

---
+++ Activities/Activity VI/PINILI_ACTIVITY_VI.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+    public class PINILI_ACTIVITY_VI{
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int num;
+
+            System.out.print("Enter number");
+            num = sc.nextInt();
+            while (num >=0){
+                System.out.println(num);
+                num --;
+            }
+
+
+        }
+    }
+
