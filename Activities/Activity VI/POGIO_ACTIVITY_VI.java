d98343a489f20d3da47b1aac1c93ec0e254f5cba
Superangelo123 <157589468+Superangelo123@users.noreply.github.com>
2024-02-12 19:02:25 +0800

    Add files via upload

---
+++ Activities/Activity VI/POGIO_ACTIVITY_VI.java
@@ -0,0 +1,20 @@
+mport java.util.Scanner;
+public class POGIO_ACTIVITY_VI{
+    public static void main (String []args){
+        Scanner sc= new Scanner(System.in);
+        
+
+        System.out.print("Enter Countdown " );
+        int count= sc.nextInt();
+
+    while(count>=0  ){
+System.out.println(count);
+count --;
+    }
+        
+        
+    
+
+        
+    }
+}
