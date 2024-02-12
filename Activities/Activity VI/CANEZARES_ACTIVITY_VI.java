ef3d41938fd457823a3d619c757b6e03f0cb4f29
Carlo P. Cañezares <156906555+CarloPC@users.noreply.github.com>
2024-02-12 19:02:01 +0800

    Add files via upload

---
+++ Activities/Activity VI/CANEZARES_ACTIVITY_VI.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class CANEZARES_ACTIVITY_VI {
+    public static void main(String []args){
+    
+    Scanner sc = new Scanner(System.in);
+        
+       System.out.print("Enter Seconds: "); 
+       int sec = sc.nextInt();
+
+        while( sec >= 0){
+            System.out.println(sec);
+            sec --;
+
+        }
+
+    }
+ }
+
