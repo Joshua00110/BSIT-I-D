52be56764c5846fc6ddec847f2cbf0f5defaf8e8
LuckyCarredo <159015794+LuckyCarredo@users.noreply.github.com>
2024-02-12 19:05:37 +0800

    CARREDO_ACTIVITY_VI.java

--- Activities/Activity VI/CARREDO_ACTIVITY_VI.java
+++ Activities/Activity VI/CARREDO_ACTIVITY_VI.java
@@ -1,7 +1,7 @@
 import java.util.Scanner
  public class CARREDO_ACTIVITY_VI {
     public static void main(String []args) {
-    Scanner sc =new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
      System.out.println("Enter the time in seconds ");
      int sec = sc.nextInt();
 
