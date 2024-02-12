386e15863b0e254262fe13844a7a25611766ceb2
shekinamariemaurin <156909161+shekinamariemaurin@users.noreply.github.com>
2024-02-12 19:03:19 +0800

    Add files via upload

---
+++ Activities/Activity VI/MAURIN_ACTIVITY_VI.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class MAURIN_ACTIVITY_VI{
+public static void main (String [] args){
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print(" Enter Seconds: ");
+    int sec = sc.nextInt();
+
+    while (sec>=0){
+
+        System.out.println(sec);
+        sec--;
+
+        }
+    }
+}
