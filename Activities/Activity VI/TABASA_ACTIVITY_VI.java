ca3f5ff3061efd6d24a9005a8e4bc423bea70d36
johnalberttaro <158041430+johnalberttaro@users.noreply.github.com>
2024-02-12 19:03:17 +0800

    Add files via upload

---
+++ Activities/Activity VI/TABASA_ACTIVITY_VI.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class TABASA_ACTIVITY_VI {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+            System.out.print("Enter your number: ");
+            int seconds = sc.nextInt();
+       
+                while(seconds >= 0 ){
+                    System.out.println(seconds);
+                    seconds--;
+            
+                }
+            
+    }
+}
