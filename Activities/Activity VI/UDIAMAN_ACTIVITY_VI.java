9e8a63df1c6226dced56356d9ba2cf6f05ea5fb6
johnkenneth1 <157591295+johnkenneth1@users.noreply.github.com>
2024-02-12 19:03:16 +0800

    Add files via upload

---
+++ Activities/Activity VI/UDIAMAN_ACTIVITY_VI.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+  public class UDIAMAN_ACTIVITY_VI{
+      public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num;
+
+        System.out.print("Enter number: ");
+        num = sc.nextInt();
+        while (num >=0) {
+            
+        
+        System.out.println(num);
+        num --;
+      }
+    }
+}
