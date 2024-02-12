d8a5ef07fefcd98848a2ba3543856cd3a2523b22
carl160 <156910039+carl160@users.noreply.github.com>
2024-02-12 19:04:30 +0800

    Add files via upload

---
+++ Activities/Activity VI/PAUSTOR_ACTIVITY_VI.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class PAUSTOR_ACTIVITY_VI{
+    public Static void main(String []args){
+        Scanner countdown+new Scanner(System.in);
+        int count;
+        System.out.println("Input number: ");
+        int count=countdown.nextInt();
+
+        while(count>=0){
+            System.out.println(count);
+            count--;{
+
+            }
+    
+
+         }
+    
+    }s           
+
+                
+            
+        
