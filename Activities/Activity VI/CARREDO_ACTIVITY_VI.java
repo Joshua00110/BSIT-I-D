d89e3ef3c9d1d2396e0d16aa141768a13add5440
LuckyCarredo <159015794+LuckyCarredo@users.noreply.github.com>
2024-02-12 19:02:43 +0800

    Add files via upload

---
+++ Activities/Activity VI/CARREDO_ACTIVITY_VI.java
@@ -0,0 +1,13 @@
+import java.util.Scanner
+ public class CARREDO_ACTIVITY_VI {
+    public static void main(String []args) {
+    Scanner sc =new Scanner(System.in);
+     System.out.println("Enter the time in seconds ");
+     int sec = sc.nextInt();
+
+     } while(sec>=0) {
+        System,out.println(count);
+        System.tou.println
+         sec--;{
+  }
+}
