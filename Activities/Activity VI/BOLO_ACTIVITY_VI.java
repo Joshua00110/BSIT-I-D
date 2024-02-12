7e60f435cffe263f5a9686750f428dbb7fabd1ea
jhonzkie201 <157588320+jhonzkie201@users.noreply.github.com>
2024-02-12 19:02:11 +0800

    Add files via upload

---
+++ Activities/Activity VI/BOLO_ACTIVITY_VI.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class BOLO_ACTIVITY_VI{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a time: ");
+        int time = sc.nextInt();
+        
+        while (time>=0){
+            System.out.println(time);
+            time --;
+            
+
+        }
+    
+}
+    
+}
