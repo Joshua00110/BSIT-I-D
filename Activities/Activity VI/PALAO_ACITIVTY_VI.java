a0d7fadbaff6f5d45a1e30c7f5897aea36bbe4c3
joshpalao20 <158041420+joshpalao20@users.noreply.github.com>
2024-02-12 19:04:45 +0800

    Add files via upload

---
+++ Activities/Activity VI/PALAO_ACITIVTY_VI.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; 
+public class PALAO_ACTIVITY_VI {
+  public static void main(String[](()args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter number of seconds:");
+    
+    int num = sc.nextInt();
+
+    while(num>=0){
+         System.out.println(num);
+         num --;
+
+    }
+  }   
+}
