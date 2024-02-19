f85c38e88da3ceadc8f2998e11c1496f3b8472b8
johnkenneth1 <157591295+johnkenneth1@users.noreply.github.com>
2024-02-19 19:05:42 +0800

    Add files via upload

---
+++ Activities/Activity VII/UDIAMAN_ACTIVITY_VII.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class UDIAMAN_ACTIVITY_VII{
+
+  public static void main(String[]args){
+
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter your factorial number");
+    int num = sc.nextInt();
+    long factorial = 1;
+    
+
+    for(long i = 1; 1 <= num; i++){
+        factorial+=1;
+    
+    
+
+    }
+    
+    System.out.println("factorial");
+
+
+
+
+
+
+
+ }
+}
+
+
+
+
+
+
+
+
+
+
+
