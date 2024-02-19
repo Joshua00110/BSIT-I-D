e43c7788753ecc26fd617c445ff9c76ccf9973a0
brianpiedad225 <158042967+brianpiedad225@users.noreply.github.com>
2024-02-19 19:03:59 +0800

    Add files via upload

---
+++ Activities/Activity VII/PIEDAD_ACTIVITY_VII.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class PIEDAD_ACTIVITY_VII {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your factorial number: ");
+        long num = input.nextInt();
+        long factorial= 1;
+        for(int x=1; x<=num;  x++){
+        factorial *=x;  
+        System.out.println(factorial);
+        }
+    }
+}
