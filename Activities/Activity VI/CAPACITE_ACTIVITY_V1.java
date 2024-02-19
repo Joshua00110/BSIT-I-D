57054680968132271dc24afcc65960c8bb58019a
joshcapz <156910254+joshcapz@users.noreply.github.com>
2024-02-19 19:06:43 +0800

    Add files via upload

---
+++ Activities/Activity VI/CAPACITE_ACTIVITY_V1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+  public class CAPACITE_ACTIVITY_VI{
+    public static void main( String []args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print(" Enter Countdown: ");
+    int count = sc.nextInt();
+    
+    while(count>=0 ) {
+    System.out.println(count);
+    sc.close();
+    count --;
+
+      }  
+  }
