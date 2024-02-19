ed2820c502b8bd9319d199f693cc00d11a6c962a
Anilov Hector P. Climaco <151500404+R3shaded@users.noreply.github.com>
2024-02-19 19:03:08 +0800

    Add files via upload

---
+++ Activities/Activity VII/CLIMACO_ACTIVITY_VII.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class CLIMACO_ACTIVITY_VII{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Number");
+        int num = sc.nextInt();
+        long factorial = 1;
+            factorial *=1;
+            int i = 1;
+            while (i <= num){
+                factorial *= i;
+                i++;
+            }
+            System.out.println("The factorial");
+        }
+    }
