496cd73b2736d2247f343cc72774c94bdd9312d8
JOHNCHRISTOPHERPIEDAD <158041306+JOHNCHRISTOPHERPIEDAD@users.noreply.github.com>
2024-02-19 19:03:37 +0800

    Add files via upload

--- Activities/Activity V/PIEDAD_ACTIVITY_V.java
+++ Activities/Activity V/PIEDAD_ACTIVITY_V.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
-public class PIEDAD_ACTIVITY_V{
-    public static void main(String[]args){
-        Scanner input= new Scanner(System.in);
-        System.out.print("Enter your age: ");
-        int age = input.nextInt();
-        if (age<=17 && age>=12){
-            System.out.println("Teen ");
-        }else if
-            (age<=64 && age>=18){
-                System.out.println("Adult ");
-            }else{
-                
-                    System.out.println("Child ");
-                
-            }
-        
-        }
+public class PIEDAD_ACTIVITY_V {
+   public static void main (String []args){
+   Scanner sc = new Scanner(System.in);
 
+   System.out.print( "Enter your age : ");
+   int age = sc.nextInt();
+
+   if( age<=11 && age>=0){
+    System.out.println(" Child ");
+    } else if ( age<=17 && age>=12){
+     System.out.println(" Teen");   
+    }else{
+      System.out.println(" Adult ");
     }
+  }
+
+}
+    
