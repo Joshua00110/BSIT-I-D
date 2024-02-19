74a2177e3d8a1792281803aee3e0192c899dd8dc
Lydivine Dalida <157588963+lydivinegakodlida04@users.noreply.github.com>
2024-02-19 19:03:28 +0800

    Add files via upload

---
+++ Activities/Activity VII/DALIDA_ACTIVITY_VII.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class DALIDA_ACTIVITY_VII {
+    
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Factorial Number: ");
+        long number = sc.nextInt();
+        long fact = 1;
+        for(long i=1; i<= number;i++){
+            fact*=i;
+        }
+        System.out.print(fact);
+        sc.close();
+        
+    }
+}
