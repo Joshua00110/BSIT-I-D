3aea7f26f5f1c49150a3ee6db43c1cce70e21954
carl160 <156910039+carl160@users.noreply.github.com>
2024-02-19 19:07:05 +0800

    Add files via upload

---
+++ Activities/Activity VII/PAUSTOR_ACTIVIT_VII.java
@@ -0,0 +1,33 @@
+import java.util.Scanner:
+public class PAUSTOR_ACTIVITY_VII{
+    public static void main(String[]args){
+        Scanner fc= new Scanner(System.in);
+        String user=fc.nextInt();
+        System.out.printIn(s:"Input your number ");
+        long num=fc.nextlong();
+        long factorial =1;
+        for(long i=1; i,num; i++){
+
+             
+
+             factorial*=i;
+             }
+
+             System.out.println(x:Factorial of"+num+5" num");
+
+             }
+
+             }
+            
+            }    
+
+   
+
+   }
+
+            
+            
+        
+
+        
+    
