32dd342b561613f8adc7a2879b0e90f01fa45a39
jhonaroldmaasin1 <158531223+jhonaroldmaasin1@users.noreply.github.com>
2024-02-19 19:03:26 +0800

    Add files via upload

---
+++ Activities/Activity VII/MAASIN_ACTIVITY_VII.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class MAASIN_ACTIVITY_VII{
+    public static void main(String[] args){
+         Scanner sc = new Scanner(System.in);
+         System.out.print("Enter Your Factorial Number: ");
+         long number = sc .nextInt();
+         long fact = 1;
+        for(long i=1; i<= number; i++){
+            fact*=i;
+        }
+        System.out.print(fact);
+    }
+}
