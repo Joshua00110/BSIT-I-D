05dc454e16b9a0b2504b0ad45d7359ae25c7fab0
xelajah17 <157589809+xelajah17@users.noreply.github.com>
2024-02-19 17:48:15 +0800

    Add files via upload

---
+++ Activities/Activity VI/QUIAPO_ACTIVITY_VI.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class QUIAPO_ACTIVITY_VI{
+    public static void main(String []args){
+        Scanner countdown = new Scanner(System.in);
+        System.out.print("Input number: ");
+        int count = countdown.nextInt();
+
+        while(count >=0){
+            System.out.println(count);  
+            count--;
+        }
+    }
+       
+}
