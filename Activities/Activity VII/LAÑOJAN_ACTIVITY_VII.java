1e94063171e608578ac16f87498b1d68b14c00c3
Iamjoseph21 <156908222+Iamjoseph21@users.noreply.github.com>
2024-02-19 19:03:11 +0800

    Add files via upload

---
+++ Activities/Activity VII/LAÑOJAN_ACTIVITY_VII.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class LAÑOJAN_ACTIVITY_VII {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+             long factorial =1;
+             System.out.println("Enter any number:");
+                long num =sc.nextInt();
+                for(long i=1;  i <= num; i++){
+                    factorial *=i;
+
+
+             }
+    System.out.println(factorial);
+    }
+
+
+
+}
