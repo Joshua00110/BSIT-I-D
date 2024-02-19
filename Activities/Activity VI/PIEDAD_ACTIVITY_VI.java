45d64500ca6edb5bc0438f917347db20d64bea0f
JOHNCHRISTOPHERPIEDAD <158041306+JOHNCHRISTOPHERPIEDAD@users.noreply.github.com>
2024-02-19 19:03:53 +0800

    Add files via upload

--- Activities/Activity VI/PIEDAD_ACTIVITY_VI.java
+++ Activities/Activity VI/PIEDAD_ACTIVITY_VI.java
@@ -1,16 +1,14 @@
 import java.util.Scanner;
 public class PIEDAD_ACTIVITY_VI{
-    public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-        int num;
-        System.out.print("Enter number: ");
-        int count= sc.nextInt();
-        while(count>=0){
-            
-            System.out.println(count);
+    public static void main(String []args){
+        Scanner countdown = new Scanner(System.in);
+        System.out.print("Input number: ");
+        int count = countdown.nextInt();
+
+        while(count >=0){
+            System.out.println(count);  
             count--;
-
-
         }
     }
+       
 }
