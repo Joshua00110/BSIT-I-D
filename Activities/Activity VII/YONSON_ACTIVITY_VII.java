8aeff04d2241c090a483280055b88a1e9a5f9b2d
Yonsonlesbert <157588466+Yonsonlesbert@users.noreply.github.com>
2024-02-19 19:03:17 +0800

    Add files via upload

---
+++ Activities/Activity VII/YONSON_ACTIVITY_VII.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class YONSON_ACTIVITY_VII {
+
+    public static void main(String[]args){
+
+
+        Scanner sc = new Scanner(System.in);
+       
+        System.out.print("Enter your Factorial number: ");
+        int num = sc.nextInt();
+        long factorial = 1;
+        
+        for(long i = 1; i <= num; i++){
+            factorial*=i;
+        
+               
+        }
+        
+        System.out.println(factorial);
+        
+
+
+    }
+}
+    
+
+   
