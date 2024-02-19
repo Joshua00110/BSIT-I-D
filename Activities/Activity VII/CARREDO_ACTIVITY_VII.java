495cdafc915e6e282170fd26d0d1eb49a206bafe
LuckyCarredo <159015794+LuckyCarredo@users.noreply.github.com>
2024-02-19 19:04:23 +0800

    Add files via upload

---
+++ Activities/Activity VII/CARREDO_ACTIVITY_VII.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CARREDO_ACTIVITY_VII{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Number");
+        int num = sc.nextInt();
+        long factorial = 1;
+            factorial *=1;
+            int i = 1;
+            while (i <=num){
+                factorial *= i;
+                i++;
+            }1
+            System.out.println("The factorial");     
+        }
+    }
+}      
