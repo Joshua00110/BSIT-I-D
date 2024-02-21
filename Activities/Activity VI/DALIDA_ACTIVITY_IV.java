710b61c511ba5eb8a8ff43813c3bde3b92a9d2ac
Lydivine Dalida <157588963+lydivinegakodlida04@users.noreply.github.com>
2024-02-21 17:53:48 +0800

    Add files via upload

---
+++ Activities/Activity VI/DALIDA_ACTIVITY_IV.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class    DALIDA_ACTIVITY_IV {
+
+   public static void main(String []args){
+   Scanner sc = new Scanner(System.in);
+   System.out.print( "Enter a number");
+   String robot = sc.nextLine();
+
+        switch(robot){
+            case "RED":
+            System.out.println("NUMBER 1");
+            break;
+            case "GREEN":
+            System.out.println("NUMBER 2");
+            break;
+            case "BLACK":
+            System.out.println("NUMBER 3");
+            break;
+            default:
+            System.out.println("mungol");
+        }
+    }
+}
+
