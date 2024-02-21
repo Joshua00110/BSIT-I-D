73b3c4f7ae4d95059b08979832b84caeadc1fb1f
Joelpalacio <157589826+Joelpalacio@users.noreply.github.com>
2024-02-21 18:22:23 +0800

    Add files via upload

---
+++ PALACIO_ACTIVITY_VII.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class  PALAO_ACTIVITY_VII{
+
+    public static void main(String[]args){
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your factorial number");
+        int num = sc.nextInt();
+        long factorial =1;
+        
+        for(long i = 1; i <= num; i++){
+            factorial+=1;
+
+
+
+
+        }    
+        
+        System.out.println("factorial");   
+
+
+
+    }
