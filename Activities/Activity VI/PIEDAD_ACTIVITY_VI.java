c36e7251d7ff63a0fbc67ad1a5e3b0ea2132dfb7
JOHNCHRISTOPHERPIEDAD <158041306+JOHNCHRISTOPHERPIEDAD@users.noreply.github.com>
2024-02-21 18:13:51 +0800

    Add files via upload

--- Activities/Activity VI/PIEDAD_ACTIVITY_VI.java
+++ Activities/Activity VI/PIEDAD_ACTIVITY_VI.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class PIEDAD_ACTIVITY_VI{
     public static void main(String []args){
         Scanner countdown = new Scanner(System.in);
-        System.out.print("Enetr your number: ");
+        System.out.print("Input number: ");
         int count = countdown.nextInt();
 
         while(count >=0){
