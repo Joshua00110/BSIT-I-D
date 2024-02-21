fdf75ff60f8db3aede01e2a200351ef03b9a12aa
brianpiedad225 <158042967+brianpiedad225@users.noreply.github.com>
2024-02-21 18:35:26 +0800

    Add files via upload

---
+++ Activities/Activity VI/PIEDAD B._ACTIVITY_VI.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class PIEDAD_ACTIVITY_VI{
+    public static void main(String []args){
+        Scanner countdown = new Scanner(System.in);
+        System.out.print("Enetr your number: ");
+        int count = countdown.nextInt();
+
+        while(count >=0){
+            System.out.println(count);  
+            count--;
+        }
+    }
+       
+}
