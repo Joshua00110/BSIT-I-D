763fd181e9f0dd689a607262a7bea5d2fa4bbd5f
jhonaroldmaasin1 <158531223+jhonaroldmaasin1@users.noreply.github.com>
2024-02-21 18:39:32 +0800

    CARREDO_ACTIVITY_V.java

--- Activities/Activity V/CARREDO_ACTIVITY_V.java
+++ Activities/Activity V/CARREDO_ACTIVITY_V.java
@@ -1,21 +1,17 @@
-import java.util.Scanner
- public class CARREDO_ACTIVITY_IIIII {
-    public static void main(String []args) {
-     Scanner ages = new Scanner.in);
-     Int age;
-     System.out.print(s:"Input your age");
-     age=ages.nextInt();
-
-     if (age < 12) {
-     System.out.println("Child");
+import java.util.Scanner;
+public class CARREDO_ACTIVITY_V {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age;
+       
+        System.out.print("Enter age: ");
+        age = sc.nextInt();
+        if (age >= 12 && age <= 17 ){
+            System.out.println("Teen");
+        }else if (age >= 0 && age <= 11) {
+                System.out.println("Child");
+            }else{
+                System.out.println("Adult"); 
+         }
     }
-    else {
-     if (age < 18) {
-     System.out.println("Teen");   
-    }
-    else {
-    System.out.println("Adult");
-     }
-   }  
-  }
 }
