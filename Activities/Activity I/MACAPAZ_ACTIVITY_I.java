b43974ff644d041419249fba186b3f4d3d667e29
kesielynmacapaz <157589229+kesielynmacapaz@users.noreply.github.com>
2024-02-21 18:00:00 +0800

    Add files via upload

---
+++ Activities/Activity I/MACAPAZ_ACTIVITY_I.java
@@ -0,0 +1,19 @@
+public class MACAPAZ_ACTIVITY_I {
+    public static void main(String[] args) {
+        int A = 5;
+        int B = 10;
+        int C = 15;
+
+        int D = A + B;
+        int E = A* B;
+        int F = B - C;
+        int G = A / B;
+
+        System.out.println("Addition:" + D);
+        System.out.println("Multiplication:" + E);
+        System.out.println("Subtraction:" + F);
+        System.out.println("Division:" + G);
+
+    }
+
+}
