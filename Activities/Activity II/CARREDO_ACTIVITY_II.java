fc29e7983bfad1ab34cb3b80e93b061938318fd1
LuckyCarredo <159015794+LuckyCarredo@users.noreply.github.com>
2024-02-23 08:37:38 +0800

    CARREDO_ACTIVITY_II.java

--- Activities/Activity II/CARREDO_ACTIVITY_II.java
+++ Activities/Activity II/CARREDO_ACTIVITY_II.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 public class CARREDO_ACTIVITY_II {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+ public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+        
         System.out.print("Enter your first number:");
         int fnum = sc.nextInt();
+        
         System.out.print("Enter your second number:");
         int snum = sc.nextInt();
 
@@ -14,8 +16,6 @@ public class CARREDO_ACTIVITY_II {
         System.out.println("Sum: " + sum);
         System.out.println("Difference: " + diff);
         System.out.println("Product: " + pro);
-
-
             
     }
     
