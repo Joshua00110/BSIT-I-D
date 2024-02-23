3cd81c6cb735453e5e3e28416385857461b3ef68
gilcabalida <158039028+gilcabalida@users.noreply.github.com>
2024-02-23 08:44:49 +0800

    Add files via upload

---
+++ Activities/Activity VI/CABALIDA_ACTIVITY_VI.java
@@ -0,0 +1,16 @@
+import java.util.*;    
+    public class CABALIDA_ACTIVITY_VI {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner (System.in);
+            System.out.print("Enter number for final countdown:");
+             int Count1 =  sc.nextInt();
+            
+            while (Count1 >=0) {
+                System.out.println(Count1);
+                Count1--;
+            }
+            
+
+
+        }
+}
