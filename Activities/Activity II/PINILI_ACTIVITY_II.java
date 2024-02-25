3777646203a3b96536321b9e645e201dd99072f7
piniliace <158040288+piniliace@users.noreply.github.com>
2024-02-25 10:19:02 +0800

    Update PINILI_ACTIVITY_II.java

--- Activities/Activity II/PINILI_ACTIVITY_II.java
+++ Activities/Activity II/PINILI_ACTIVITY_II.java
@@ -3,14 +3,14 @@ public class PINILI_ACTIVITY_II{
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         System.out.print("First number: ");
-        int fnum = sc.nextlnt();
+        int fnum = sc.nextInt();
         System.out.print("Second number: ");
-        int snum = sc.nextlnt();
+        int snum = sc.nextInt();
 
-        int sum = fnum + sum;
-        int diff = fnum - sum;
-        int product = fnum * sum;
-        int quotient = fnum / sum;
+        int sum = fnum + snum;
+        int diff = fnum - snum;
+        int product = fnum * snum;
+        int quotient = fnum / snum;
 
         System.out.println("Sum = " + sum);
         System.out.println("Difference = " + diff);
