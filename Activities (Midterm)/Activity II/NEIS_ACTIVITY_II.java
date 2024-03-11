a77296f042559f312dacdc214dbc8697bbfda24c
nealeJanric <158532556+nealeJanric@users.noreply.github.com>
2024-03-11 18:42:59 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity II/NEIS_ACTIVITY_II.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+    public class NEIS_ACTIVITY_II {
+
+        public static void main(String []args){
+            Scanner sc = new Scanner (System.in);
+
+            double []score = new double [10];
+            double average;
+            double sum =0;
+
+            for(int i= 0; i < score.length; i++){
+            System.out.print("Enter score"+(i+1)+":");
+            score[i] = sc.nextInt();
+            sum + score[i];
+            
+        }
+        double average=sum / score.length;
+        System.out.printIn(average);
+    }
+}
