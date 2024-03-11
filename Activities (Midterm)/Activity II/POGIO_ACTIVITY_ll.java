e5b73b2c88005e03d42b8c410772913d6adb9614
Superangelo123 <157589468+Superangelo123@users.noreply.github.com>
2024-03-11 18:39:48 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity II/POGIO_ACTIVITY_ll.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class BALONGCAS_ACTIVITY_II {
+
+    public static void main(String[] args) {
+        Scanner activity = new Scanner(System.in);
+        double[] scores = new double[10];
+        
+        for(int s = 0; s < 10; s++){
+            System.out.println("Enter Score" + (s + 1) + ": ");
+            scores[s] = activity.nextDouble();
+
+        }
+
+        double sum = 0;
+        for(double s: scores){
+            sum += s;
+        }
+        double average = sum / scores.length;
+        System.out.println("Average Score: " + average);
+        
+    }
+}
