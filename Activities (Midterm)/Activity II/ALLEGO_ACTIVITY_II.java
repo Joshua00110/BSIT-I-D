4e22ca68b18dbfef2e3687e73951a86b326c99c4
allegofaith <156910544+allegofaith@users.noreply.github.com>
2024-03-11 18:44:28 +0800

    Update ALLEGO_ACTIVITY_II.java

--- Activities (Midterm)/Activity II/ALLEGO_ACTIVITY_II.java
+++ Activities (Midterm)/Activity II/ALLEGO_ACTIVITY_II.java
@@ -2,22 +2,22 @@ import java.util.Scanner;
 
 public class ALLEGO_ACTIVITY_II {
   public static void main (String [] args){
-   Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
  
-   int[] scores = new int [10];
+    int[] scores = new int [10];
  
-   int sum = 0;
-   for (int i = 0; i <scores.length; i++){
-   System.out.print(" Enter score" + (i + 1) + " :");
-   scores[i] = sc.nextInt();
-   sum += scores [i];
-}
+    int sum = 0;
+    for (int i = 0; i <scores.length; i++){
+    System.out.print(" Enter score" + (i + 1) + " :");
+    scores[i] = sc.nextInt();
+    sum += scores [i];
+    }
 
-  double average = (double) sum / scores.length;
+    double average = (double) sum / scores.length;
 
-  System.out.print(average);
+    System.out.print(average);
  
- }
+  }
 }
 
  
