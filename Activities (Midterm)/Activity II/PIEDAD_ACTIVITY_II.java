f6cdb9ec8c7a857a4a5526b0f8a8d5b69a54dd3e
JOHNCHRISTOPHERPIEDAD <158041306+JOHNCHRISTOPHERPIEDAD@users.noreply.github.com>
2024-03-11 18:43:38 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity II/PIEDAD_ACTIVITY_II.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PIEDAD_ACTIVITY_II{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int[]scores = new int[10];
+        double sum = 0;
+        System.out.println("Enter 10 scores: ");
+        for(int i = 0; i < 10; i++) {
+            System.out.print("Enter score " + (i + 1) + ":");       
+            scores[i] = sc.nextInt();
+            sum += scores[i];
+        }
+        double average = sum / 10;
+
+        System.out.println("Your Average score is: " + average);
+    }
+}
