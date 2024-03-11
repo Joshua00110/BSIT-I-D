980932ab417d84f48c6adf0949c4304085503486
Leonises <157588921+calunsagleonises@users.noreply.github.com>
2024-03-11 18:40:17 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity II/CALUNSAG_ACTIVITY_II.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class CALUNSAG_ACTIVITY_II {
+    public static void main(String []args){
+        Scanner sc = new Scanner (System.in);
+
+        double []score = new double[10];
+        double average;
+        double sum=0;
+
+        for(int i=0; i<score.length;i++){
+          System.out.println("Enter your score " +(i+1)+": ");
+          score [i] = sc.nextInt();
+          sum += score[i];
+
+        }
+        average=sum/10;
+         System.out.println(average);
+    }
+}
+
