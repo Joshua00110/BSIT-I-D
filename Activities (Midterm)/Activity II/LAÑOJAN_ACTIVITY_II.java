e96905e296494f30ed28bc35c666844b9e8c1e9e
Iamjoseph21 <156908222+Iamjoseph21@users.noreply.github.com>
2024-03-11 18:42:02 +0800

    LAÑOJAN_ACTIVITY_II.java

--- Activities (Midterm)/Activity II/LAÑOJAN_ACTIVITY_II.java
+++ Activities (Midterm)/Activity II/LAÑOJAN_ACTIVITY_II.java
@@ -1,23 +1,19 @@
- import java.util.Scanner;
+import java.util.Scanner;
 
-public class LAÑOJAN_ACTIVITY_II{
-    public static void main(String []args){
-        Scanner sc = new Scanner(System.in);
-    int[]scores = new int[10];
-    int sum = 0;
+public class LAÑOJAN_ACTIVITY_II {
+    public static void main(String[] args) {
+        Scanner arr = new Scanner(System.in);
+        int [] scores = new int[10];
+        int sum = 0;
 
-        System.out.println("Enter 10 scores: ");
-        for(int i = 0; i < scores.length; i++) {
-            System.out.print("Enter score " + (i + 1) + ":");       
-            scores[i] = sc.nextInt();
+        for (int i = 0; i < scores.length; i++) {
+            System.out.print("Enter score " + (i + 1) + ": ");
+            scores[i] = arr.nextInt();
             sum += scores[i];
         }
-        double average = sum / scores.length;
 
-        System.out.println("Your Average score is: " + average);
-
+        double average = (double) sum / scores.length;
+        System.out.println("Average score: " + average);
     }
-   
-} 
-    
+}
 
