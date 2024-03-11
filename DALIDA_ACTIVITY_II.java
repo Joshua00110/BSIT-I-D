4e8c222164d1ac395535e79d1fd98147bbeda17a
Lydivine Dalida <157588963+lydivinegakodlida04@users.noreply.github.com>
2024-03-11 18:39:58 +0800

    Add files via upload

---
+++ DALIDA_ACTIVITY_II.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DALIDA_ACTIVITY_II{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+
+       double [] scores = new double[10];
+       double sum = 0;
+       double average;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Enter score" + (i + 1) + ": ");
+            scores[i] = sc.nextInt();
+            sum += scores [1];            
+        }
+        average = sum / scores.length;
+
+        System.out.println("The total Average score:" + average);
+    }
+}
