a222cb780119fcf14ac9d6a2a6cb8c8c1be57f68
allegofaith <156910544+allegofaith@users.noreply.github.com>
2024-03-18 18:32:02 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity III/ALLEGO_ACTIVITY_III.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class ALLEGO_ACTIVITY_III {
+    public static void main(String [] args){
+    Scanner sc = new Scanner(System.in);
+
+    int[][] scores = new int[5][5]; 
+       
+    System.out.println("Enter scores: ");
+
+        for (int i = 0; i < scores.length; i++) {
+            for (int k = 0; k < scores[i].length; k++) {
+                scores[i][k] = sc.nextInt();
+            }
+        }
+
+    
+       int sum = 0;
+       int count = 0;
+        for (int i = 0; i < scores.length; i++) {
+            for (int k= 0;k < scores[i].length; k++) {
+                sum += scores[i][k];
+                count++;
+            }
+       } 
+    double average = (double) sum / count;
+        System.out.println("Average: " + average);
+
+    }
+    
+}
