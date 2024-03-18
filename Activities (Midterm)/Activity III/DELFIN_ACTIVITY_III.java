e81bf36a73acfb7c475d3cea93774fbf50c63e60
warr90 <158041134+warr90@users.noreply.github.com>
2024-03-18 18:31:30 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity III/DELFIN_ACTIVITY_III.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class DELFIN_ACTIVITY_III {
+   public static void main(String [] args){
+   
+    int [][] score = new int [5][5];
+
+        System.out.println("ENTER THE SCORE:");
+
+        for (int x = 0; x < score. length; x++){
+            for (int y = 0; y < score.length; y++){
+                System.out.print("Enter score for row" + (x + 1) +", column" + (y+1) + ":");
+                score[x][y] = sc.nextInt();
+            }
+        }
+ 
+        int sum = 0;
+        int count = 0;
+        for (int x = 0; x < score. length; x++) {
+            for (int y = 0; y < score.length; y++) {
+                 sum += score [x][y]; 
+                 count++;
+            }
+        }
+ 
+        double average = (double) sum / count;
+        System.out.println("The Average of the Scored: " + average);
+    }
+}
