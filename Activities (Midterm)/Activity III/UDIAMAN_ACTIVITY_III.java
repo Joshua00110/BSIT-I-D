1abd1bada2dbab0528ff92ea6d4e015d18e55c0e
johnkenneth1 <157591295+johnkenneth1@users.noreply.github.com>
2024-03-20 16:03:23 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity III/UDIAMAN_ACTIVITY_III.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class UDIAMAN_ACTIVITY_III {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+       
+        int[][] scores = new int[5][5];
+
+        
+        System.out.println("Enter scores:");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                scores[i][j] = scanner.nextInt();
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                sum += scores[i][j];
+            }
+        }
+        double average = (double) sum / (5 * 5);
+        System.out.println("Average of all scores: " + average);
+        scanner.close();
+    }
+}
