fe20b7339c12f5277e80b89d171148ff1a782256
GiaGraceVansie <158039956+GiaGraceVansie@users.noreply.github.com>
2024-03-22 09:20:44 +0800

    Create AGUILOS_ACTIVITY_III.cpp

---
+++ Activities (Midterm)/Activity III/AGUILOS_ACTIVITY_III.cpp
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class CANEZARES_ACTIVITY_III {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int[][] scores = new int[5][5];
+        
+        int sum = 0;
+        int num = 0;
+    
+        for (int i = 0; i < scores.length; i++) {
+            for (int j = 0; j < scores[i].length; j++) {
+                System.out.print("Enter scores: ");
+                scores[i][j] = sc.nextInt();
+                sum += scores[i][j];
+                num++;
+            }
+        }
+        double average =  sum / num;
+        
+        System.out.println("Average score: " + average);
+    }
+}
