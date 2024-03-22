432ea4eae0a6dbe9d85efa0648c9e1324b78a625
gilcabalida <158039028+gilcabalida@users.noreply.github.com>
2024-03-22 08:33:25 +0800

    Update CABALIDA_ACTIVITY_II.java

--- Activities (Midterm)/Activity II/CABALIDA_ACTIVITY_VII.java
+++ Activities (Midterm)/Activity II/CABALIDA_ACTIVITY_VII.java
@@ -1,16 +1,1 @@
-import java.util.Scanner;
-public class CABALIDA_ACTIVITY_VII {0
-    
-                   public static void main(String []args){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Your Factorial Number: ");
-        long number = sc.nextInt();
-        long fact = 1;
-        for(long x=1; x<= number;x++){
-            fact*=x;
-        }
-        System.out.print(fact);
-        sc.close();
-        
-    }
-}
+
