7074dd70d347cb7dfde652f44dc82267f4c29c47
Joelpalacio <157589826+Joelpalacio@users.noreply.github.com>
2024-04-15 18:50:49 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/PALACIO_ACTIVITY_I.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class PALACIO_ACTIVITY_I{
+    public static void main(String []args){
+
+        double length = cal;
+        double width = cal2;
+
+        double Area = length*width;
+
+        System.out.print("Area is: "+Area);
+
+    }
+    public static double cal{
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter length: ");
+        return = sc.nextDouble();
+    }
+    public static double cal2{
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter width: ");
+        return = sc.nextDouble();
+    }
+
+}
