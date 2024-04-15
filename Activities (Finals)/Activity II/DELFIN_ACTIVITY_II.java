c50140908d8b004cdbe8d317d29e98627c637ac7
warr90 <158041134+warr90@users.noreply.github.com>
2024-04-15 18:56:57 +0800

    DELFIN_ACTIVITY_II.java

--- Activities (Finals)/Activity II/DELFIN_ACTIVITY_II.java
+++ Activities (Finals)/Activity II/DELFIN_ACTIVITY_II.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 
 public class DELFIN_ACTIVITY_II {
-    static int getfeet(){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the feet: ");
-    int feet = getfeet();
-    int inches = feet * 12;
-    System.out.println(inches);
-    } 
-   public static void main(String [] args){
+    static void getfeet(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the feet: ");
+        double feet = sc.nextDouble();
+        double inches = feet * 12;
+        System.out.println(inches);
+    }
+    public static void main(String []args){
         getfeet();
-        
-    }  
+    }
 }
