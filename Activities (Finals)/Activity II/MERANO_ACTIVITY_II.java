e85743c673d29c2bcd21e268bfc645d7f63b84e6
Kathleen Merano <157017491+mkathleenn@users.noreply.github.com>
2024-04-15 18:50:25 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/MERANO_ACTIVITY_II.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class MERANO_ACTIVITY_II {
+
+    static void conversion(int feet){
+        int res = feet * 12;
+        System.out.println("Inches: "+ res);
+
+    }
+
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter feet: ");
+        int feet = sc.nextInt();
+        conversion (feet);
+    }
+    
+}
