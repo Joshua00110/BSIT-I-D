be551870a59b1f4e71dc881ed9548c083280736a
Joelpalacio <157589826+Joelpalacio@users.noreply.github.com>
2024-04-15 18:51:53 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/PALACIO_ACTIVITY_II.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class PALACIO_ACTIVITY_II    {
+ public static void main (String[] args) {
+
+    double feet = getfeet ();
+   double inches = feet * 12;
+    System.out.println("The inches are " +inches);
+ }
+
+    static double getfeet() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter feet:");
+        return sc.nextDouble();
+    }
+
