fffd0a9fc62b6e6a1fc94926a22d091e34e7c42d
jevs7714 <157568642+jevs7714@users.noreply.github.com>
2024-04-15 18:13:39 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/BARNIDO_ACTIVITY_I.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class BARNIDO_ACTIVITY_I {
+    public static void main(String[] args) {
+      
+     static double length{
+      Scanner sc= new Scanner(System.in);
+      System.out.println("Enter length: ");
+      return sc.nextDouble();
+
+     }
+
+     static double width{
+      Scanner sc= new Scanner(System.in);
+      System.out.println("Enter width: ");
+      return sc.nextDouble();
+
+     }
+
+     static double area{
+      Scanner sc= new Scanner(System.in);
+      System.out.println("Enter width: ");
+      return sc.nextDouble();
+
+     }
+
+     double length = getlength();
+     double width = getwdith();
+     double area= getarea();
+     System.out.println("Area: " +area);
+    
+    }
+  }
