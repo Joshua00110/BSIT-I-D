5320ef25e9f2ed866e112d376874609abf620625
Daveboi123 <157589344+Daveboi123@users.noreply.github.com>
2024-04-15 18:53:00 +0800

    Update ARINGAY_ACTIVITY_II.java

---
+++ Activities (Finals)/Activity II/ARINGAY_ACTIVITY_II.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ARINGAY_ACTIVITY_II {
+
+    public static void feetToInches(double feet){
+        double inches = feet * 12;
+        System.out.println("The result in inches is: " + inches);
+
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of feet: ");
+        double feet = scanner.nextDouble();
+        feetToInches(feet);
+    }
+}
