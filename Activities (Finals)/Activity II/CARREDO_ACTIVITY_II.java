cb5bddbab46ab376b3a26ab6b815cc14a4813c86
LuckyCarredo <159015794+LuckyCarredo@users.noreply.github.com>
2024-04-15 18:53:58 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/CARREDO_ACTIVITY_II.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class CARREDO_ACTIVITY_II {
+    static void facelessvoid(double feet){
+        double ursa = feet*12;
+        System.out.println(ursa);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your feet: ");
+        double feet = sc.nextInt();
+        System.out.print("Inches: " );
+        facelessvoid ( feet);
+    }
+}
