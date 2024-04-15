a437b2e3fe63992924a31ae7eb53847629d7cbe4
kesielynmacapaz <157589229+kesielynmacapaz@users.noreply.github.com>
2024-04-15 18:51:36 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/MACAPAZ_ACTIVITY_II.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+    public class MACAPAZ_ACTIVITY_II {
+        static void convert(int feet){
+            int res = feet*12;
+            System.out.println("Inches: "+res);
+        }
+        public static void main(String []args){
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter the feet: ");
+            int feet = input.nextInt();
+            convert(feet);
+        }
+        
+    }
