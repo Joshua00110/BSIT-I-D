b259948f70e88b794af374cdd009d4c47931783b
jevs7714 <157568642+jevs7714@users.noreply.github.com>
2024-04-15 18:50:57 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/BARNIDO_ACTIVITY_II.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class BARNIDO_ACTIVITY_II {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+       static void feet(int a){
+            double res=a*12;
+            System.out.println(res);
+       }
+
+       System.out.println("Enter feet: ");
+       
+        feet(x);
+        int x=sc.nextInt();
+
+    }
+}
