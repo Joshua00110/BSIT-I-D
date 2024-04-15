5ce6dd4cbcf87c08c87813cdf1e9d4e32733880d
johnkenneth1 <157591295+johnkenneth1@users.noreply.github.com>
2024-04-15 18:55:40 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/UDIAMAN_ACTIVITY_II.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class UDIAMAN_ACTIVITY_II {
+        static void Feet(double feet) {
+            System.out.println(feet * 12);
+    
+        }
+    
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the feet : ");
+            int feet = sc.nextInt();
+            System.out.print("Inches : ");
+            Feet(feet);
+    
+            sc.close();
+        }
+    }
