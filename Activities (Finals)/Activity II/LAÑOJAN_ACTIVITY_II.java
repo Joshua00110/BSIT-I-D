5f0b588825d5ec4cf896b02689389230711e0265
Iamjoseph21 <156908222+Iamjoseph21@users.noreply.github.com>
2024-04-15 18:50:23 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/LAÑOJAN_ACTIVITY_II.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class LAÑOJAN_ACTIVITY_II    {
+ public static void main (String[] args) {
+
+    double feet = getfeet ();
+   double inches = feet * 12;
+    System.out.println("The inches are " +inches);
+ }
+
+    static double getfeet() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter feet:");
+        return sc.nextDouble();
+    }
+
+
+    
+
+    
+    }
+
