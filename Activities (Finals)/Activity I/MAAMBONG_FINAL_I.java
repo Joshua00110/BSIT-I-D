976e9eddb264b6bd04cf155f33ff357886a30e3b
Joshua00110 <156908549+Joshua00110@users.noreply.github.com>
2024-04-15 18:13:17 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/MAAMBONG_FINAL_I.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class MAAMBONG_FINAL_I {
+
+    static double length() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Length: ");
+        return sc.nextDouble();
+    }
+
+    static double width() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Width: ");
+        return sc.nextDouble();
+    }
+
+    public static void main(String[] args) {
+
+        double result = width() * length();
+        System.out.println("The Area is " + result);
+    }
+
+}
