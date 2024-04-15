bc72c99453a18ced66a18d84ac5da8f6d784c6f5
Daveboi123 <157589344+Daveboi123@users.noreply.github.com>
2024-04-15 18:58:35 +0800

    Update ARINGAY_ACTIVITY_I.java

--- Activities (Finals)/Activity I/ARINGAY_ACTIVITY_I.java
+++ Activities (Finals)/Activity I/ARINGAY_ACTIVITY_I.java
@@ -2,35 +2,35 @@ import java.util.Scanner;
 
 public class ARINGAY_ACTIVITY_I {
 
-    static double inputlength(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input the length of the rectangle: ");
-        return sc.nextDouble();
+    public static double inputLength() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the Length: ");
+        return scanner.nextDouble();
     }
-
-    static double inputwidth() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input the width of the rectangle: ");
-        return sc.nextDouble;
+    public static double inputWidth() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the Width: ");
+        return scanner.nextDouble();
     }
-
-    static double calculatearea(double length, double width){
+    public static double calculateArea(double length, double width){
         return length * width;
     }
-    static void Area(double area){
-        System.out.println("Area of the rectangle:" + Area);
-        double Area = calculatearea(length, widdth);
-
+    public static void displayArea(double area) {
+        System.out.println("Area of the rectangle is: " + area);
     }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double length = inputlength();
-        double width = inputwidth();
+        double length = inputLength();
+        double width = inputWidth();
 
-    double area = calculatearea(length, widdth);
+        double area = calculateArea(length, width);
 
+        displayArea(area);
+    }
+       
 }
-}
+
+
 
 
 
