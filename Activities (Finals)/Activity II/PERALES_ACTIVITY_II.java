d3ebc19117b25398e246279fe69008f5ffcb26bd
alissonperales <157588373+alissonperales@users.noreply.github.com>
2024-04-15 18:52:00 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/PERALES_ACTIVITY_II.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class PERALES_FINAL_II {
+    static void Feet(double feet) {
+        System.out.println(feet * 12);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the feet : ");
+        int feet = sc.nextInt();
+        System.out.print("Inches : ");
+        Feet(feet);
+
+        sc.close();
+    }
+}
+        
+    
+
+    
+       
+
+
+    
+
