00a21f326859bed735c0f90a4c9e58d91ea73867
Iamjoseph21 <156908222+Iamjoseph21@users.noreply.github.com>
2024-04-15 18:12:06 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/LAÑOJAN_ACTIVITY_I.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class LAÑOJAN_ACTIVITY_I {
+    public static void main(String[] args) {
+        double length= getlength();
+        double width= getwidth();
+        double area = length*width;
+        System.out.println("Area="+area);
+    }
+    static double getlength () {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter a Length:");
+        return sc.nextDouble();
+    }
+    static double getwidth () {
+        Scanner sc= new Scanner(System.in);
+        
+        System.out.println("Enter a width:");
+        return sc.nextDouble();
+    }
+}
