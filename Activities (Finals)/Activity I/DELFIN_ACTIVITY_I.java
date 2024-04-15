cba20eb0da433bb7c7c1f5dd29a31c961cec0b9f
warr90 <158041134+warr90@users.noreply.github.com>
2024-04-15 18:50:35 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/DELFIN_ACTIVITY_I.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DELFIN_ACTIVITY_I {
+    public static void main(String[] args){
+     int length  = getLength();
+     int area  = length * width;
+     int width  = getWidth();
+     System.out.println("Area = : " + area);    
+   }
+     static int getLength () {
+     Scanner sc = new Scanner(System.in);
+     System.out.print("Enter the leght of the rectangle: ");
+     return sc.nextInt();
+    }
+     static int getWidth();
+     Scanner sc = new Scanner(System.in);   
+     System.out.print("Enter the width of the rectangle: ");
+     return sc.nextInt();
+    }
+}
+  
+
+
