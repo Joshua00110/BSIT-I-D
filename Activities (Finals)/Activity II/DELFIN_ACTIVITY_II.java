a601da7f95f4eeb1c7a5d3d741bd6b35642391d9
warr90 <158041134+warr90@users.noreply.github.com>
2024-04-15 18:51:51 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/DELFIN_ACTIVITY_II.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class DELFIN_ACTIVITY_II {
+    static int getfeet(){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the feet: ");
+    int feet = getfeet();
+    int inches = feet * 12;
+    System.out.println(inches);
+    } 
+   public static void main(String [] args){
+        getfeet();
+        
+    }  
+}
