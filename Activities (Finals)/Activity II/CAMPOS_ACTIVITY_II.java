8c47c780ee5ea25161b67416cc070278abbaf0b8
Kelrizz26 <158041133+Kelrizz26@users.noreply.github.com>
2024-04-15 18:52:15 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/CAMPOS_ACTIVITY_II.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class CAMPOS_ACTIVITY_II {
+    static void Feet(double feet) {
+        System.out.println(feet * 12);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the feet : ");
+        int feet = sc.nextInt();
+        System.out.print("Inches : ");
+        Feet(feet);
+
+        sc.close();
+    }
+}
