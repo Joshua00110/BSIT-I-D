cc91a13b739422db2c75a195a479ef05904f25ba
JayEstre <156909272+JayEstre@users.noreply.github.com>
2024-04-15 18:51:16 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/MAGSAYO_ACTICVITY_II.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class MAGSAYO_ACTICVITY_II {
+    static void facelessvoid(double feet){
+        double ursa = feet*12;
+        System.out.println(ursa);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your feet: ");
+        double feet = sc.nextInt();
+        System.out.print("Inches: " );
+        facelessvoid ( feet);
+
+    }
+}
