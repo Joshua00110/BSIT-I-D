172a3ce583faf011bdff7eef2e05f92d1081ae79
xelajah17 <157589809+xelajah17@users.noreply.github.com>
2024-04-25 21:05:29 -0700

    Add files via upload

---
+++ Activities (Midterm)/Activity III/QUIAPO_ACTIVITY_III.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class QUIAPO_ACTIVITY_III {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[][] scores = new int[5][5];
+
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println("Enter Scores " + (i + 1) + ":");
+            for (int y = 0; y < scores[i].length; y++) {
+                scores[i][y] = input.nextInt();
+            }
+        }
+
+        double sum = 0;
+        int count = 0;
+        for (int x = 0; x < scores.length; x++) {
+            for (int y = 0; y < scores[x].length; y++) {
+                sum += scores[x][y];
+                count++;
+            }
+        }
+
+        double average = sum / count;
+        System.out.println("Total Score Average: " + average);
+    }
+}
