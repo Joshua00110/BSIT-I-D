58811bde6db3eef77ae94695b43cc56d08c35b69
xelajah17 <157589809+xelajah17@users.noreply.github.com>
2024-04-25 20:42:40 -0700

    Add files via upload

---
+++ Activities (Finals)/Activity I/QUIAPO_ACTIVITY_I.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class MERANO_ACTIVITY_I {
+
+    static int width () {
+        System.out.print("Enter the width: ");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+
+    static int length (){
+        System.out.print("Enter the length: ");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+
+    public static void main(String [] args){
+       
+       int result = width()*length();
+       System.out.println("Area of the rectangle is "+ result);
+       
+    }    
+}
