bf89acdaf8f308886cb5b40b73eeea93a693c235
joshpalao20 <158041420+joshpalao20@users.noreply.github.com>
2024-05-06 18:21:24 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/import java.util.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class PALAO_ACTIVITY_I {
+    public static void main(String []args){
+
+        double length = inputL();
+        double width = inputW();
+
+        double Area = length * width;
+
+        System.out.println("The Area of the rectangle is: " + Area);
+
+    }
+
+    public static double inputL(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Length: ");
+        return sc.nextDouble();
+    }
+    public static double inputW(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Width: ");
+        return sc.nextDouble();
+    }
+
+
+}
