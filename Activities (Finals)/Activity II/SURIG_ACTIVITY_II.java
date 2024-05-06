7b08903e96bc63f0d9268970b8ae560900deeb0a
crislyn18 <157588791+crislyn18@users.noreply.github.com>
2024-05-06 02:56:50 -0700

    Add files via upload

---
+++ Activities (Finals)/Activity II/SURIG_ACTIVITY_II.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class SURIG_ACTIVITY_II {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your feet: ");
+        double feet = sc.nextDouble();
+        System.out.println("Inches: ");
+        FeettoInches(feet);
+        
+    }
+   
+   static void FeettoInches (double feet){
+        double res = feet * 12;
+        System.out.println(res);
+    }
+   
+}
