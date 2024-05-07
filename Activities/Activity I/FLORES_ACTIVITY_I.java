522852773e718103c86a276066a05e9229e87ddd
JoshuaFlores15 <157085353+JoshuaFlores15@users.noreply.github.com>
2024-05-07 19:44:09 +0800

    Add files via upload

---
+++ Activities/Activity I/FLORES_ACTIVITY_I.java
@@ -0,0 +1,15 @@
+public class FLORES_ACTIVITY_I {
+    public static void main(String[] args) {
+        int x = 10, y = 20, z = 30;
+
+        int sum = x + y + z;
+        int diff = z - y -x;
+        int prod = x * y * z;
+        int qout = z / y/ x;
+
+        System.out.println("Sum: "+sum);
+        System.out.println("Diff: "+diff);
+        System.out.println("Prod: "+prod);
+        System.out.println("Qout:"+qout);
+    }
+}
